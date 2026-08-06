aaf86f9f4b3b875c5d8dd5a8789c57875563b28f
velmurugan <velmurugan4426@gmail.com>
2026-08-06 20:50:00 +0530

    day 1

---
+++ Day01/Recursion.java
@@ -0,0 +1,55 @@
+/*
+//print names 5 times using recursion
+import java.util.*;
+public class Recursion{
+    public static void main(String args[]){
+        String name="vel";
+        print(name,5);
+    }
+    static void print(String name,int count){
+        if(count<=0){
+            return ;
+        }
+        System.out.println(name);
+        count--;
+        print(name,count);
+    }
+    
+}
+*/
+
+
+/*
+//print number from reverse using recursion
+public class Recursion{
+    public static void main(String[] args) {
+         print(5);
+    }
+    static void print(int n){
+        if(n<=0){
+            return;
+        }
+        System.out.println(n);
+        
+        print(--n);//here n-- wont work post decrement so again and again 5 will run
+    }
+
+}
+*/
+
+
+//recursion (print after recursion statement)
+import java.util.*;
+public class Recursion{
+    public static void main(String[] args) {
+        int i=1,n=5;
+        print(i,n);
+    }
+    static void print(int i,int n){
+        if(n<i){
+            return;
+        }
+        print(i,n-1);
+        System.err.println(n);
+    }
+}
